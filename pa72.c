206b6f01a16f9968b210dadd79fc34df4591e1ad
zhanghuicheng <zhanghuicheng329@gmail.com>
2024-10-09 22:42:37 +0800

    pta 72

---
+++ pa72.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+    char c;
+    int i = 1;
+    for (; scanf;) {
+        scanf("%c", c);
+        if (c == ' ') {
+            i = 0;
+        }
+        if (c >= 'a' && c <= 'z' && i == 0) {
+            printf("%c", c - 32);
+        } else {
+            printf("%c", c);
+        }
+        if (scanf == 0);
+        {
+            break;
+        }
+    }
+}
