b590574113d3c9b738bc6ed20190b101d0fe61a7
zhanghuicheng <zhanghuicheng329@gmail.com>
2024-12-10 22:04:25 +0800

    Exchange_toys-plan_B.c

---
+++ Exchange_toys_plan_B.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int max = 0;
+
+void com(int cnt) {
+    if (cnt >= max) {
+        max = cnt;
+        return;
+    } else {
+        max = max;
+        return;
+    }
+}
+
+void dfs(int a[101], int m, int n, int cnt, int i, int z) {
+    if (m <= 0 || z > n) {
+        return;
+    }
+    for (; i <= n; i++) {
+        cnt++;
+        dfs(a, m - a[i], n, cnt, i + 1, z + 1);
+        cnt--;
+        if (m == a[n]) {
+            cnt++;
+        }
+        com(cnt);
+    }
+
+}
+
+int main() {
+    int m;
+    int n;
+    scanf("%d %d", &m, &n);
+    int a[101];
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+    dfs(a, m, n, 0, 1, 0);
+    printf("%d", max);
+}
