371465862e642d1550abcecda9d362411af43fb9
zhanghuicheng <zhanghuicheng329@gmail.com>
2024-12-10 22:47:24 +0800

    Exchange_toys_correction.c

---
+++ Exchange_toys_correction.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int max = 0;
+
+void com(int cnt) {
+    if (cnt >= max) {
+        max = cnt;
+        return;
+    } else {
+        max = max;
+        return;
+    }
+}
+
+void dfs(int a[101], int m, int n, int cnt, int i, int z) {
+    if (m < 0 || z > n) {
+        return;
+    }
+    for (; i <= n; i++) {
+        dfs(a, m - a[i], n, cnt + 1, i + 1, z + 1);
+        if (i==n && m == a[n]) {
+            cnt++;
+        }
+        com(cnt);
+    }
+
+}
+
+int main() {
+    int m;
+    int n;
+    scanf("%d %d", &m, &n);
+    int a[101];
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+    dfs(a, m, n, 0, 1, 0);
+    printf("%d", max);
+}
