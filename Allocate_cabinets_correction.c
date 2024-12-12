6f21178916c97acccada6537277d6ab77afd889e
zhanghuicheng <zhanghuicheng329@gmail.com>
2024-12-12 22:25:20 +0800

    Allocate_cabinets_correction.c.c

---
+++ Allocate_cabinets_correction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int i, j;
+    scanf("%d %d", &i, &j);
+    int m = j % 4;
+    if (m == 0) {
+        printf("%d ", j / 4);
+    } else {
+        printf("%d ", (j / 4) + 1);
+    }
+    int a = j % 4;
+    if (a == 0) {
+        a = 4;
+    }
+    printf("%d", (a - 1) * 10 + i);
+}
