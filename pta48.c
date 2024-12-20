83675ed7ec133e1c46cb9e70e796bfb4442fc662
zhanghuicheng <zhanghuicheng329@gmail.com>
2024-12-20 19:25:53 +0800

    pta48.c

--- pta48.c
+++ pta48.c
@@ -1,24 +1,3 @@
-#include<stdio.h>
-
-int main() {
-    int a, b;
-    char c;
-    double summ, sum;
-    scanf("%d %d %c", &a, &b, &c);
-    if (b == 90) {
-        summ = a * 6.95;
-    }
-    if (b == 93) {
-        summ = a * 7.44;
-    }
-    if (b == 97) {
-        summ = a * 7.93;
-    }
-    if (c == 'm') {
-        sum = summ * 0.95;
-    }
-    if (c == 'e') {
-        sum = summ * 0.97;
-    }
-    printf("%.2f", sum);
-}
+//
+// Created by 章辉丞 on 2024/10/21.
+//
