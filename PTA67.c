64a7e638e9f5f8a2a8e4c4e6f626481323389823
zhanghuicheng <zhanghuicheng329@gmail.com>
2024-12-25 22:47:02 +0800

    PTA67.c.c

--- PTA67.c
+++ PTA67.c
@@ -1,30 +1,15 @@
 #include<stdio.h>
 
 int main() {
-    int m, n;
-    int sum = 0;
-    scanf("%d %d", &m, &n);
-    if (n >= 6 && m <= n && m > 1 && n <= 10000) {
-        for (int i = m; i <= n; i++) {
-            sum = 0;
-            for (int x = 1; x < i; x++) {
-                if ((i % x) == 0) {
-                    sum += x;
-                }
-            }
-            if (sum == i) {
-                printf("%d = 1", i);
-                for (int y = 2; y < i; y++) {
-                    if ((i % y) == 0) {
-                        printf(" + %d", y);
-                    }
-                    if (y == (i - 1)) {
-                        printf("\n");
-                    }
-                }
-            }
-        }
-    } else {
-        printf("None");
+    int n;
+    scanf("%d", &n);
+    int i;
+    int ch;
+    for( i = 0; i < n; i++){
+       scanf("%d", &ch);
+       int m = ch%10;
+       int a[10000000000];
+       a[0] = m;
+       for( )
     }
 }
